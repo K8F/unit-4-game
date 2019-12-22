57fe1856249d1fb1f66090af825428461b580c90
Kate Foust <kate.e.foust@gmail.com>
2019-12-22 13:39:48 -0600

    random number appears in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,17 @@
 //global variables
+var firstNumber
+
 
 
 
 
 function startGame(){
     //new random number between 19-120
+
+    var randomNumber = {};
+        randomNumber=Math.floor(Math.random()*(120-19 + 1) + 19);
+        $("div#random-number").text(randomNumber);
+        console.log(randomNumber)
 
     //generate new numbers for crystals between 1-12
     var crystalValues = {};
@@ -13,8 +20,10 @@ function startGame(){
         yellowCrystal = Math.floor(Math.random()*12+1);
         greenCrystal = Math.floor(Math.random()*12+1);
 
-
-        console.log(greenCrystal)
+console.log(redCrystal)
+console.log(blueCrystal)
+console.log(yellowCrystal)
+console.log(greenCrystal)
     //when play wins, add to win score
 
     //when player loses, add to lose score
@@ -27,11 +36,9 @@ startGame()
 
 
 //buttons are clickable
-$( "#red-crystal" ).click(function() {
-    alert( "Handler for .click() called." );
-  });
-  $( "#blue-crystal" ).click(function() {
-    alert( "Handler for .click() called." );
+$( "#red-crystal" ).on("click () called." );
+$( "#blue-crystal" ).click(function() {
+    alert( "the number is " + blueCrystal );
   });
   $( "#yellow-crystal" ).click(function() {
     alert( "Handler for .click() called." );
