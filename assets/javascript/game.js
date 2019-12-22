1d4ee249606f874149b87ad57aa2b0a2aefbad9a
Kate Foust <kate.e.foust@gmail.com>
2019-12-22 14:50:22 -0600

    wins/losses works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,67 @@
 //global variables
-var firstNumber
+
+//numbers to add
+var firstNumber =0;
+var secondNumber =0;
+
+//user total score
+var totalScore=0;
+
+//user wins/losses
+var wins=0;
+var losses=0;
+
+//display wins and losses
+$("div#wins").text("Wins: " + wins);
+$("div#losses").text("Losses: " + losses);
 
 
 
+ //when play wins, add to win score
 
+ function winner() {
+    alert("You Won!!");
+    wins++;
+    $("div#wins").text("Wins: " + wins);
+    startGame();
+}
+
+//when player loses, add to lose score
+function loser() {
+    alert("You Lose!!");
+    losses++;
+    $("div#losses").text("Losses: " + losses);
+    startGame();
+}
+
+$("#image1").on("click", function() {
+    userTotal = userTotal + num1;
+    console.log("New userTotal " + userTotal);
+    $("#score").text(userTotal);
+
+    if (userTotal === random) {
+        winner()
+    }
+
+    else if (userTotal > random) {
+        loser()
+    } 
+})
+winner();
+
+//first time user clicks crystal, crystal number displays under "your score"
+
+//every time user clicks crystal, crystal number adds to the current total under "your score"
+//if total score matches random number, user wins
+//if total score is higher than random number, user loses
 
 function startGame(){
+    
+    //total score resets to 0
+
+    $("div#score-number").text(firstNumber);
+
+    
     //new random number between 19-120
 
     var randomNumber = {};
@@ -24,11 +80,6 @@ console.log(redCrystal)
 console.log(blueCrystal)
 console.log(yellowCrystal)
 console.log(greenCrystal)
-    //when play wins, add to win score
-
-    //when player loses, add to lose score
-
-
 }
 
 startGame()
